d42e307c491ea4ea01e12486c031ba9735bf7be4
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-28 22:03:43 -0300

    app

--- app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MainActivity.java
+++ app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MainActivity.java
@@ -1,14 +1,28 @@
 package br.com.etecia.cidadeinteligentesustentavel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    List<DescLixo> listaLixo;
+    RecyclerView idRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idRecyclerView = findViewById(R.id.idListaLixo);
+
+        listaLixo = new ArrayList<>();
+
+        listaLixo.add(
+                new DescLixo("Causas Ambientais",
+                        "Desmatamento",
+                        R.drawable.natureza));
     }
 }
