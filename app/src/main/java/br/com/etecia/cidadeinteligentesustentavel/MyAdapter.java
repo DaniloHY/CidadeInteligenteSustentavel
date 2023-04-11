bbdf6614ecf73e811b47e071171593acd56352a7
DaniloHY <nilo.11.dhy@gmail.com>
2023-04-11 20:01:34 -0300

    Adaptador

--- app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MyAdapter.java
+++ app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MyAdapter.java
@@ -1,6 +1,9 @@
 package br.com.etecia.cidadeinteligentesustentavel;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -24,17 +27,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @NonNull
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(mContexto);
+        view = inflater.inflate(R.layout.modelo, parent, false);
+
+        return new ViewHolder(view);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.idCausaLixo.setText(listaLixo.get(position).getTitulo());
+        holder.idImgLixo.setImageResource(listaLixo.get(position).getImagem());
+        holder.idCardViewLixo.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(mContexto, ApresentaLixoActivity.class);
+
+                intent.putExtra("Titulo", listaLixo.get(position).getTitulo());
+                intent.putExtra("Descricao", listaLixo.get(position).getDescricao());
+                intent.putExtra("ImagemFilme", listaLixo.get(position).getImagem());
+
+                mContexto.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaLixo.size();
     }
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView idCausaLixo;
