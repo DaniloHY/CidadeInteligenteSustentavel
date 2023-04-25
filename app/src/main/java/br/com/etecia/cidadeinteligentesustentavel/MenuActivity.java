3d68f03afd7b888453e39bad03d8b88dd60e3679
DaniloHY <nilo.11.dhy@gmail.com>
2023-04-25 19:44:29 -0300

    Tentando Arrumar

--- app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MenuActivity.java
+++ app/src/main/java/br/com/etecia/cidadeinteligentesustentavel/MenuActivity.java
@@ -1,16 +1,30 @@
 package br.com.etecia.cidadeinteligentesustentavel;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
+
+    MaterialToolbar toolbar;
+
+    DrawerLayout drawerLayout;
+
+    NavigationView navigationView;
+
+    ActionBarDrawerToggle toggle;
+
 
     List<DescLixo> listaLixo;
     RecyclerView idRecyclerView;
@@ -31,5 +45,27 @@ public class MenuActivity extends AppCompatActivity {
         idRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
 
         idRecyclerView.setHasFixedSize(true);
+
+        toolbar = findViewById(R.id.idToolBar);
+        drawerLayout = findViewById(R.id.idDrawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
+        //Clique no menu para abrir e fechar o NavigationView
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout,
+                toolbar,
+                R.string.opennav,
+                R.string.closenav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.idContainerNav, new ImportFragment()).commit();
+
+            navigationView.setCheckedItem(R.id.mImport);
     }
-}
+}}
